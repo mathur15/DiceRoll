269223edcf0205ccea078f39edd5cfd830502f77
mathur15 <som.mathur1997@gmail.com>
2020-06-26 15:34:50 -0400

    Initial set up of logic complete, need to update the score

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-/*
-GAME RULES:
+init()
+generateRandomNumber()
+activePlayer = 0
+scores = [0,0]
+function init(){
+    document.querySelector(".dice").style.display = "none"
+    document.querySelector("#score-0").innerHTML = '0'
+    document.querySelector("#score-1").innerHTML = '0'
+    document.querySelector("#current-0").innerHTML = '0'
+    document.querySelector("#current-1").innerHTML = '0'
+}
+function generateRandomNumber(){
+    return Math.floor(Math.random() * 6) + 1
+}
+document.querySelector(".btn-new").addEventListener("click",function(){
+    init()
+})
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+document.querySelector(".btn-roll").addEventListener("click",function(){
+    var number = generateRandomNumber()
+    document.querySelector(".dice").src = "dice-"+number+".png"
+    document.querySelector(".dice").style.display = "initial"
+    updateScore(activePlayer,number)
+})
 
-*/
+
+
+
+
+
