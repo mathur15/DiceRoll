6c93110e000496e495b4294bcb8c738266e9b69e
mathur15 <som.mathur1997@gmail.com>
2020-06-27 02:02:19 -0400

    Changes to check winner in multiple locations and showing the results on the UI

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-init()
 activePlayer = 0
 scores = [0,0]
+init()
 function init(){
     document.querySelector(".dice").style.display = "none"
     scores = [0,0]
+    activePlayer = 0
     document.querySelector("#score-0").innerHTML = '0'
     document.querySelector("#score-1").innerHTML = '0'
-    document.querySelector("#current-0").innerHTML = '0'
-    document.querySelector("#current-1").innerHTML = '0'
+    document.querySelector("#current-0").textContent = '0'
+    document.querySelector("#current-1").textContent = '0'
 }
 function generateRandomNumber(){
     return Math.floor(Math.random() * 6) + 1
@@ -17,6 +18,10 @@ function updateCurrentScore(activePlayer,number){
     current_score = Number(current_score)
     current_score = current_score + number
     document.querySelector("#current-"+activePlayer).innerHTML = current_score
+    if(checkWinner(current_score)){
+        updateGlobalScore(activePlayer)
+        declareWinner(current_score)
+    }
 }
 function updateGlobalScore(activePlayer){
     var current_score = document.querySelector("#current-"+activePlayer).textContent
@@ -24,23 +29,27 @@ function updateGlobalScore(activePlayer){
     document.querySelector("#score-"+activePlayer).textContent = ''
     document.querySelector("#score-"+activePlayer).textContent = scores[activePlayer]
     document.querySelector("#current-"+activePlayer).innerHTML = '0'
-    player_winner_status = checkWinner(activePlayer)
-    if(player_winner_status){
-        console.log("Player"+" "+(activePlayer+1)+" "+"is the winner.")
-        init()
-    }
 }
 function toggleActivePlayer(activePlayer){
-    //console.log(activePlayer === 0? 1:0);
     return activePlayer === 0? 1:0
 }
-function checkWinner(){
-    if(scores[activePlayer] >= 100){
+function checkWinner(current_score){
+    if(scores[activePlayer]+current_score >= 20){
         return true
     }
     return false
 }
+function declareWinner(){
+    document.querySelector("#name-"+activePlayer).textContent = "WINNER"
+    document.querySelector(".btn-roll").style.display = "none"
+    document.querySelector(".btn-hold").style.display = "none"
+    document.querySelector(".dice").style.display = "none"
+}
 document.querySelector(".btn-new").addEventListener("click",function(){
+    document.querySelector("#name-0").textContent = "Player 1"
+    document.querySelector("#name-1").textContent = "Player 2"
+    document.querySelector(".btn-roll").style.display = "initial"
+    document.querySelector(".btn-hold").style.display = "initial"
     init()
 })
 
@@ -59,8 +68,6 @@ document.querySelector(".btn-roll").addEventListener("click",function(){
     }
 })
 document.querySelector(".btn-hold").addEventListener("click",function(){
-    //update the current_score and global score
-    //update the current active player
     updateGlobalScore(activePlayer)
     document.querySelector(".player-"+activePlayer+"-panel").classList.remove('active')
     activePlayer = toggleActivePlayer(activePlayer)
