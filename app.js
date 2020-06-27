8e5137f036b40001785ef5697a4701475815e1fc
mathur15 <som.mathur1997@gmail.com>
2020-06-27 17:20:41 -0400

    Make sure two consecutive 6's wipes out score

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 activePlayer = 0
 scores = [0,0]
+previous_roll = 0 //to check two consecutive sixes
 init()
 function init(){
     document.querySelector(".dice").style.display = "none"
     scores = [0,0]
     activePlayer = 0
+    previous_roll = 0
     document.querySelector("#score-0").innerHTML = '0'
     document.querySelector("#score-1").innerHTML = '0'
     document.querySelector("#current-0").textContent = '0'
@@ -57,14 +59,17 @@ document.querySelector(".btn-roll").addEventListener("click",function(){
     var number = generateRandomNumber()
     document.querySelector(".dice").src = "dice-"+number+".png"
     document.querySelector(".dice").style.display = "initial"
-    if(number!=1){
-        updateCurrentScore(activePlayer,number)
-    }
-    else{
+
+    if(number==1 || (previous_roll ==6 && number ==6)){
+        previous_roll = 0
         document.querySelector("#current-"+activePlayer).textContent='0'
         document.querySelector(".player-"+activePlayer+"-panel").classList.remove('active')
         activePlayer = toggleActivePlayer(activePlayer)
         document.querySelector(".player-"+activePlayer+"-panel").classList.add('active')
+    }
+    else{
+        previous_roll = number
+        updateCurrentScore(activePlayer,number)
     }
 })
 document.querySelector(".btn-hold").addEventListener("click",function(){
